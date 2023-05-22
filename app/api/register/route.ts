04131cdc35210cbaac02f8c5b2bf3aac641ddfe1
Vy <thanh.tran@kamora.vn>
2023-05-22 22:37:42 +0700

    implement register feature

---
+++ app/api/register/route.ts
@@ -0,0 +1,42 @@
+import bcrypt from 'bcrypt';
+import prismadb from "@/lib/prismadb";
+import {NextResponse, NextRequest} from "next/server";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+	try {
+		const {email, name, password} = await (req.json());
+
+		const existingUser = await prismadb.user.findUnique({
+			where: {
+				email
+			}
+		});
+
+		if (existingUser) {
+			return NextResponse.json({
+				error: 'Email taken'
+			}, {
+				status: 422
+			})
+		}
+
+		const hashedPassword = await bcrypt.hash(password, 12);
+
+		const user = await prismadb.user.create({
+			data: {
+				email,
+				name,
+				hashedPassword,
+				image: '',
+				emailVerified: new Date()
+			}
+		})
+
+		return NextResponse.json({ user }, { status: 200 })
+	} catch (error) {
+		console.log(error);
+		return NextResponse.json( {
+			status: 422
+		})
+	}
+}
